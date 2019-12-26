f6f27a4076219804ca9339d4857a84d7f077c84d
Stefan Østergaard <stefan.ostergaard@tunstall.com>
2019-12-26 20:24:09 +0100

    DateTimePicker Issue

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,10 +1,15 @@
-import React, { useState, FormEvent, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Segment, Form, Button, Grid } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/stores/activityStore';
 import { RouteComponentProps } from 'react-router-dom';
+import { Form as FinalForm, Field } from 'react-final-form';
+import TextInput from '../../../app/common/form/TextInput';
+import TextAreaInput from '../../../app/common/form/TextAreaInput';
+import SelectInput from '../../../app/common/form/SelectInput';
+import { category } from '../../../app/common/options/categoryOptions';
 
 interface DetailParams {
   id: string;
@@ -52,85 +57,88 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
     activity.id.length,
   ]);
 
-  const handleSubmit = () => {
-    if (activity.id.length !== 0) {
-      editActivity(activity).then(() =>
-        history.push(`/activities/${activity.id}`)
-      );
-    } else {
-      let newActivity = {
-        ...activity,
-        id: uuid(),
-      };
-      createActivity(newActivity).then(() =>
-        history.push(`/activities/${newActivity.id}`)
-      );
-    }
-  };
+  // const handleSubmit = () => {
+  //   if (activity.id.length !== 0) {
+  //     editActivity(activity).then(() =>
+  //       history.push(`/activities/${activity.id}`)
+  //     );
+  //   } else {
+  //     let newActivity = {
+  //       ...activity,
+  //       id: uuid(),
+  //     };
+  //     createActivity(newActivity).then(() =>
+  //       history.push(`/activities/${newActivity.id}`)
+  //     );
+  //   }
+  // };
 
-  const handleInputChange = (
-    event: FormEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = event.currentTarget;
-    setActivity({ ...activity, [name]: value });
+  const handleFinalFormSubmit = (values: any) => {
+    console.log(values);
   };
 
   return (
     <Grid>
       <Grid.Column width={10}>
         <Segment clearing>
-          <Form onSubmit={handleSubmit}>
-            <Form.Input
-              onChange={handleInputChange}
-              placeholder="Title"
-              name="title"
-              value={activity.title}
-            ></Form.Input>
-            <Form.TextArea
-              onChange={handleInputChange}
-              placeholder="Description"
-              name="description"
-              value={activity.description}
-            ></Form.TextArea>
-            <Form.Input
-              onChange={handleInputChange}
-              placeholder="Category"
-              name="category"
-              value={activity.category}
-            ></Form.Input>
-            <Form.Input
-              onChange={handleInputChange}
-              type="datetime-local"
-              name="date"
-              placeholder="Date"
-              value={activity.date}
-            ></Form.Input>
-            <Form.Input
-              onChange={handleInputChange}
-              placeholder="City"
-              name="city"
-              value={activity.city}
-            ></Form.Input>
-            <Form.Input
-              onChange={handleInputChange}
-              placeholder="Venue"
-              name="venue"
-              value={activity.venue}
-            ></Form.Input>
-            <Button
-              loading={submitting}
-              floated="right"
-              positive
-              type="submit"
-              content="Submit"
-            />
-            <Button
-              onClick={() => history.push('/activities')}
-              floated="right"
-              type="button"
-              content="Cancel"
-            />
-          </Form>
+          <FinalForm
+            onSubmit={handleFinalFormSubmit}
+            render={({ handleSubmit }) => (
+              <Form onSubmit={handleSubmit}>
+                <Field
+                  name="title"
+                  placeholder="Title"
+                  value={activity.title}
+                  component={TextInput}
+                />
+                <Field
+                  placeholder="Description"
+                  name="description"
+                  rows={3}
+                  value={activity.description}
+                  component={TextAreaInput}
+                ></Field>
+                <Field
+                  placeholder="Category"
+                  options={category}
+                  name="category"
+                  value={activity.category}
+                  component={SelectInput}
+                ></Field>
+                <Field
+                  name="date"
+                  placeholder="Date"
+                  value={activity.date}
+                  component={TextInput}
+                ></Field>
+                <Field
+                  placeholder="City"
+                  name="city"
+                  value={activity.city}
+                  component={TextInput}
+                ></Field>
+                <Field
+                  placeholder="Venue"
+                  name="venue"
+                  value={activity.venue}
+                  component={TextInput}
+                ></Field>
+                <Button
+                  loading={submitting}
+                  floated="right"
+                  positive
+                  type="submit"
+                  content="Submit"
+                />
+                <Button
+                  onClick={() => history.push('/activities')}
+                  floated="right"
+                  type="button"
+                  content="Cancel"
+                />
+              </Form>
+            )}
+          />
         </Segment>
       </Grid.Column>
     </Grid>
